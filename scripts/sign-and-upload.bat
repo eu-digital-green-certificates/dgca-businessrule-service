748069545db80687df80efacfb7a95dd1d7ef556
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-06-28 11:17:39 +0200

    Update Sign-And-Upload Script

--- scripts/sign-and-upload.bat
+++ scripts/sign-and-upload.bat
@@ -9,16 +9,18 @@ SET TLS_CERT="auth.pem"
 
 REM DO NOT CHANGE ANYTHING BELOW THIS!
 
-echo Purging temporary directory
-
 echo Search rule files and sign with Upload Certificate and Upload to DGCG
 
-for /f "usebackq delims=|" %%f in (`dir /s/b rule.json`) do (
-	@echo off
-	echo Processing JSON file %%f
-	dgc signing sign-string -c %SIGNING_CERT% -k %SIGNING_KEY% -i %%f -o tmp.cms
-	curl -w -s --request POST "%DGCG_ENDPOINT%" --header "Content-Type: application/cms-text" --header "Accept: application/json" --data-binary @tmp.cms --cert %TLS_CERT% --key %TLS_KEY% -o curl.log
-)
+for /f "usebackq delims=|" %%f in (`dir /s/b rule.json`) do (call :upload %%f)
 
 echo deleting temporary file
-rm -f tmp.cms
+del -f tmp.cms
+
+goto :eof
+
+:upload
+echo Processing JSON file %1
+call dgc signing sign-string -c %SIGNING_CERT% -k %SIGNING_KEY% -i "%1" -o tmp.cms
+call curl --no-progress-bar --request POST "%DGCG_ENDPOINT%" --header "Content-Type: application/cms-text" --header "Accept: application/json" --data-binary @tmp.cms --cert %TLS_CERT% --key %TLS_KEY%
+echo.
+echo.
