ee42aaa115434c372da4f4069fcb1c8de2ef4994
Artur T <81235055+a-trzewik@users.noreply.github.com>
2021-06-29 11:08:38 +0200

    fix/binary to hex

--- src/main/java/eu/europa/ec/dgc/businessrule/utils/BusinessRulesUtils.java
+++ src/main/java/eu/europa/ec/dgc/businessrule/utils/BusinessRulesUtils.java
@@ -20,35 +20,27 @@
 
 package eu.europa.ec.dgc.businessrule.utils;
 
+import eu.europa.ec.dgc.utils.CertificateUtils;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.util.encoders.Hex;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class BusinessRulesUtils {
+    private final CertificateUtils certificateUtils;
 
     /**
      * returns SHA-256 Thumbprint of the data (hex encoded).
      */
     public String calculateHash(String data) throws NoSuchAlgorithmException {
-        return calculateHash(data.getBytes(StandardCharsets.UTF_8));
+        return certificateUtils.calculateHash(data.getBytes(StandardCharsets.UTF_8));
     }
 
-    /**
-     * returns SHA-256 Thumbprint of the data (hex encoded).
-     */
-    public String calculateHash(byte[] data) throws NoSuchAlgorithmException {
-        byte[] certHashBytes = MessageDigest.getInstance("SHA-256").digest(data);
-        String hexString = new BigInteger(1, certHashBytes).toString(16);
-
-        if (hexString.length() == 63) {
-            hexString = "0" + hexString;
-        }
-
-        return hexString;
-    }
 }
