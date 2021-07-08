38b5a14555d13a076bfbac92157e6a848a070cfb
m.schulte <M.Schulte@t-systems.com>
2021-07-08 18:44:27 +0200

    disable cors for testing

--- src/main/java/eu/europa/ec/dgc/businessrule/config/WebSecurityConfig.java
+++ src/main/java/eu/europa/ec/dgc/businessrule/config/WebSecurityConfig.java
@@ -9,7 +9,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().and().csrf().disable();
     }
 
 }
